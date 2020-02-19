e6c08a18dddf8d7858e031ad3f423d6dc74aea65
Wojciech <olowek92@gmail.com>
2020-02-19 16:20:03 +0100

    Task-1 done

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,13 @@
-//Twój kod
+const express = require("express");
+const app = express();
+
+app.get("/file/:number/:secondNumber", (req, res) => {
+  const { number, secondNumber } = req.params;
+  const sum = parseInt(number) + parseInt(secondNumber);
+
+  res.send(`Wynik: ${sum}`);
+});
+
+app.listen(2000, () => {
+  console.log("Serwer uruchomiony na porcie 2000");
+});
