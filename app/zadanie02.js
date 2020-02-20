c7f6c284f2839e75ce8568790f70676ac0cb2902
Wojciech <olowek92@gmail.com>
2020-02-20 16:32:17 +0100

    Task-2 done

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,28 @@
-//Twój kod
+const express = require("express");
+const app = express();
+
+let name = "";
+
+app.get("/name/set/:name", (req, res) => {
+  name = req.params.name;
+
+  res.send("Podane imię to: " + name);
+});
+
+app.get("/name/show", (req, res) => {
+  res.send("Podane imię to: " + name);
+});
+
+app.get("/name/check", (req, res) => {
+  let text = "";
+  if (name.length > 0) {
+    text = "Zapisane imię to: " + name;
+  } else {
+    text = "Brak zapisanych imion!";
+  }
+  res.send(text);
+});
+
+app.listen(3000, () => {
+  console.log("Serwer uruchomiony na porcie 3000");
+});
